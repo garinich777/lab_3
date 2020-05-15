a1b316581d40485fd4d9f2013836f5a1031eccf0
Admin <777programmer777@gmil.com>
2020-05-15 19:07:13 +0300

    Реализован блок Model
    В Model реализона функция GetFunc, которая возвращает значение заданой функции

---
+++ lab_3/Model/MathModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab_3.Model
+{
+    static class MathModel
+    {
+        static double GetFunc(double x, double a)
+        {
+            return Math.Sqrt((Math.Pow(a, 2) * Math.Pow(x, 2) - Math.Pow(x, 4)) / Math.Pow(a, 2));
+        }
+    }
+}
