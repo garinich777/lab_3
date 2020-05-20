49b314aa9f84155a7adcb553531d3cab5c0ea48f
garinich777 <62898482+garinich777@users.noreply.github.com>
2020-05-21 00:09:44 +0300

    Реализован моульный тест

---
+++ lab_3.Test/MathTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using lab_3.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace lab_3.Test
+{
+    [TestClass]
+    public class MathTest
+    {
+        [TestMethod]
+        public void TestFunc()
+        {
+            double x = 8;
+            double a = 16;
+            double ExpectedValue = 6.928;
+
+            Assert.AreEqual(ExpectedValue, Math.Round(MathModel.GetFunc(x, a), 3));
+        }
+    }
+}
---
+++ lab_3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("lab_3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab_3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d0a25ccc-8084-4d7f-861d-cd857b5f078a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
